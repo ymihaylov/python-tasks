a610f47e8035ebed12147aa14da77760641b9dca
Yavor Mihaylov <yavor.st.m@gmail.com>
2016-12-30 09:34:22 +0200

    Add notes for lecture 0

---
+++ lectures_notes/0-intro.py
@@ -0,0 +1,8 @@
+import sys
+
+print(sys.version)
+
+def real_knights(*knights):
+    return ['Sir {}'.format(knight.capitalize()) for knight in knights]
+
+print(real_knights('John', 'snow', 'Rudolf'))
