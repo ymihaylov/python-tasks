32008017ad17b4d5169d2124abb8432a16ae36ea
Yavor Mihaylov <yavor.st.m@gmail.com>
2017-01-06 00:05:04 +0200

    Add solution for 01_2012_lets_eat

---
+++ tasks/01_2012_lets_eat/first_solution.py
@@ -0,0 +1,24 @@
+def count_of_spam(number):
+    count = 0
+
+    while number % 3 == 0:
+        count += 1
+        number //= 3
+
+    return count
+
+
+def prepare_meal(number):
+    count = count_of_spam(number)
+    meal = (count * 'spam ').strip()
+
+    if (number % 5 == 0):
+        if (meal == ''):
+            meal = 'eggs'
+        else:
+            meal += ' and eggs'
+
+    return meal
+
+
+print(prepare_meal(5))
