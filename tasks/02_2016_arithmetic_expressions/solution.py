801c1c4c1a4e37ff9b7d386515955726bc1d540c
Yavor Mihaylov <yavor.st.m@gmail.com>
2017-01-17 23:46:33 +0200

    Expressions work with constants and variables

--- tasks/02_2016_arithmetic_expressions/solution.py
+++ tasks/02_2016_arithmetic_expressions/solution.py
@@ -2,7 +2,8 @@ class Constant:
     def __init__(self, value):
         self.value = value
 
-    def evaluate(self):
+    def evaluate(self, variables):
+        # print(variables)
         return self.value
 
 
@@ -10,17 +11,14 @@ class Variable:
     def __init__(self, name):
         self.name = name
 
-    def evaluate(self, **variables):
-        return variables[self.name]
+    def evaluate(self, variable):
+        return variable[self.name]
 
 
 class Operator:
     def __init__(self, symbol, function):
         self.symbol = symbol
         self.function = function
-
-    def evaluate():
-        pass
 
     def __str__(self):
         return self.symbol
@@ -34,17 +32,21 @@ class Expression:
         self.operator = expression_structure[1]
         self.rhs = expression_structure[2]
 
-    def evaluate(self, **variables):
-        return self.operator.function(
-            self.lhs.evaluate(),
-            self.rhs.evaluate()
-        )
+    def get_value_by_object(self, obj, variables):
+        if isinstance(obj, Variable):
+            return obj.evaluate(variables)
+        elif isinstance(obj, Constant):
+            return obj.evaluate()
 
-    # def evaluate(self, **variables):
-    #     return self.operator.function(
-    #         variables[self.lhs.name],
-    #         variables[self.rhs.name]
-    #     )
+    def evaluate(self, **variables):
+        # print(variables)
+        # print(self.lhs.evaluate(variables))
+        # self.lhs_value = self.get_value_by_object(self.lhs, variables)
+        # self.rhs_value = self.get_value_by_object(self.rhs, variables)
+        return self.operator.function(
+            self.lhs.evaluate(variables),
+            self.rhs.evaluate(variables)
+        )
 
 
 def create_constant(value):
@@ -64,13 +66,15 @@ def create_expression(expression_structure):
 
 
 plus = create_operator('+', lambda lhs, rhs: lhs + rhs)
-minus = create_operator('-', lambda lhs, rhs: lhs - rhs)
-times = create_operator('*', lambda lhs, rhs: lhs * rhs)
-six = create_constant(6)
+# six = create_constant(6)
+six = create_variable('x')
 nine = create_constant(9)
-expression = create_expression((six, times, nine))
+expression = create_expression((six, plus, nine))
+print(expression.evaluate(x=6))
 
-print(expression.evaluate())
+print(isinstance(six, Variable))
+# print(expression.evaluate())
+
 # plus = create_operator('+', lambda lhs, rhs: lhs + rhs)
 # x = create_variable('x')
 # y = create_variable('y')
