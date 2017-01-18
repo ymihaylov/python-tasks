2f285bbcc695471098badb6d1c1a3b1416c7c25a
Yavor Mihaylov <yavor.st.m@gmail.com>
2017-01-18 02:11:29 +0200

    Update tests

--- tasks/02_2016_arithmetic_expressions/solution.py
+++ tasks/02_2016_arithmetic_expressions/solution.py
@@ -1,18 +1,39 @@
-class Constant:
+class Arithmetic:
+    def __add__(self, other):
+        plus = Operator('+', lambda self, other: self + other)
+        return Expression(self, plus, other)
+
+    def __sub__(self, other):
+        sub = Operator('-', lambda self, other: self - other)
+        return Expression(self, sub, other)
+
+    def __mul__(self, other):
+        mul = Operator('*', lambda self, other: self * other)
+        return Expression(self, mul, other)
+
+    @property
+    def variable_names(self):
+        return tuple()
+
+
+class Constant(Arithmetic):
     def __init__(self, value):
         self.value = value
 
-    def evaluate(self, variables):
-        # print(variables)
+    def evaluate(self, **variables):
         return self.value
 
 
-class Variable:
+class Variable(Arithmetic):
     def __init__(self, name):
         self.name = name
 
-    def evaluate(self, variable):
+    def evaluate(self, **variable):
         return variable[self.name]
+
+    @property
+    def variable_names(self):
+        return tuple(self.name,)
 
 
 class Operator:
@@ -24,29 +45,21 @@ class Operator:
         return self.symbol
 
 
-class Expression:
-    def __init__(self, expression_structure):
-        self.expression_structure = expression_structure
-
-        self.lhs = expression_structure[0]
-        self.operator = expression_structure[1]
-        self.rhs = expression_structure[2]
-
-    def get_value_by_object(self, obj, variables):
-        if isinstance(obj, Variable):
-            return obj.evaluate(variables)
-        elif isinstance(obj, Constant):
-            return obj.evaluate()
+class Expression(Arithmetic):
+    def __init__(self, lhs, operator, rhs):
+        self.lhs = lhs
+        self.operator = operator
+        self.rhs = rhs
 
     def evaluate(self, **variables):
-        # print(variables)
-        # print(self.lhs.evaluate(variables))
-        # self.lhs_value = self.get_value_by_object(self.lhs, variables)
-        # self.rhs_value = self.get_value_by_object(self.rhs, variables)
         return self.operator.function(
-            self.lhs.evaluate(variables),
-            self.rhs.evaluate(variables)
+            self.lhs.evaluate(**variables),
+            self.rhs.evaluate(**variables)
         )
+
+    @property
+    def variable_names(self):
+        return self.lhs.variable_names + self.rhs.variable_names
 
 
 def create_constant(value):
@@ -62,21 +75,33 @@ def create_operator(symbol, function):
 
 
 def create_expression(expression_structure):
-    return Expression(expression_structure)
+    stack = []
 
+    for element in expression_structure:
+        if isinstance(element, tuple):
+            stack.append(create_expression(element))
+        else:
+            stack.append(element)
 
-plus = create_operator('+', lambda lhs, rhs: lhs + rhs)
-# six = create_constant(6)
-six = create_variable('x')
+    if len(stack) == 3:
+        expression = Expression(stack[0], stack[1], stack[2])
+        stack.clear()
+        stack.append(expression)
+
+    return stack[0]
+
+six = create_constant(6)
 nine = create_constant(9)
-expression = create_expression((six, plus, nine))
-print(expression.evaluate(x=6))
+times = create_operator('*', lambda lhs, rhs: lhs * rhs)
+minus = create_operator('-', lambda lhs, rhs: lhs - rhs)
+plus = create_operator('+', lambda lhs, rhs: lhs + rhs)
+x = create_variable('x')
+y = create_variable('y')
 
-print(isinstance(six, Variable))
-# print(expression.evaluate())
+expression = create_expression(
+    (six, times, ((x, minus, y), plus, nine))
+)
 
-# plus = create_operator('+', lambda lhs, rhs: lhs + rhs)
-# x = create_variable('x')
-# y = create_variable('y')
-# added_expression = create_expression((x, plus, y))
-# print(added_expression.evaluate(x=5, y=3))
+(x + 3 * (y - 2)).evaluate(x=1, y=4)
+
+# print(expression.variable_names)
--- tasks/02_2016_arithmetic_expressions/tests.py
+++ tasks/02_2016_arithmetic_expressions/tests.py
@@ -4,24 +4,46 @@ import solution
 
 
 class SampleTest(unittest.TestCase):
-    # def test_five_plus_three(self):
-    #     plus = solution.create_operator('+', lambda lhs, rhs: lhs + rhs)
-    #     x = solution.create_variable('x')
-    #     y = solution.create_variable('y')
-    #     added_expression = solution.create_expression((x, plus, y))
-    #     self.assertEqual(added_expression.evaluate(x=5, y=3), 8)
+    def test_five_plus_three(self):
+        plus = solution.create_operator('+', lambda lhs, rhs: lhs + rhs)
+        x = solution.create_variable('x')
+        y = solution.create_variable('y')
+        added_expression = solution.create_expression((x, plus, y))
+        self.assertEqual(added_expression.evaluate(x=5, y=3), 8)
 
-    # def test_operators(self):
-    #     y = solution.create_variable('y')
-    #     twelve = solution.create_constant(12)
-    #     expression = y + twelve
-    #     self.assertEqual(expression.evaluate(y=3), 15)
+    def test_operators(self):
+        y = solution.create_variable('y')
+        twelve = solution.create_constant(12)
+        expression = y + twelve
+        self.assertEqual(expression.evaluate(y=3), 15)
+
+    def test_variable_names(self):
+        x = solution.create_variable('x')
+        y = solution.create_variable('y')
+        eight = solution.create_constant(8)
+
+        expression = x + y + eight
+        self.assertEqual(expression.variable_names, ('x', 'y'))
+
+    def test_complex_expression(self):
+        six = solution.create_constant(6)
+        nine = solution.create_constant(9)
+        times = solution.create_operator('*', lambda lhs, rhs: lhs * rhs)
+        minus = solution.create_operator('-', lambda lhs, rhs: lhs - rhs)
+        plus = solution.create_operator('+', lambda lhs, rhs: lhs + rhs)
+        x = solution.create_variable('x')
+        y = solution.create_variable('y')
+
+        expression = solution.create_expression(
+            (six, times, ((x, minus, y), plus, nine))
+        )
+
+        self.assertEqual(expression.evaluate(x=5, y=2), 72)
 
     def test_variable_evaluation(self):
         self.assertEqual(solution.create_variable('x').evaluate(x=42), 42)
 
     def test_constant_evaluation(self):
-        # self.assertEqual(solution.create_variable('x').evaluate(x=42), 42)
         self.assertEqual(solution.create_constant(5).evaluate(), 5)
         self.assertEqual(solution.create_constant(10 + 5).evaluate(), 15)
 
