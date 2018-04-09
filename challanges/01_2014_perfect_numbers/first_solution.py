f63062a93a45a09f40b3c0bc0396fbbdd5c83ca4
Yavor Mihaylov <yavor.st.m@gmail.com>
2018-04-09 23:48:43 +0300

    Add gitignore and update challanges solutions

--- challanges/01_2014_perfect_numbers/first_solution.py
+++ challanges/01_2014_perfect_numbers/first_solution.py
@@ -1,7 +1,10 @@
 def is_perfect(number):
     divisors = []
-    for i in range(1, number / 2):
-        if (number % i == 0):
+    for i in range(1, number):
+        if number % i == 0:
             divisors.append(i)
 
     return sum(divisors) == number
+
+
+print(is_perfect(6))
--- challanges/01_2014_perfect_numbers/short_solution.py
+++ challanges/01_2014_perfect_numbers/short_solution.py
@@ -1,2 +1,3 @@
 def is_perfect(number):
     return sum([n for n in range(1, number) if number % n == 0]) == number
+
