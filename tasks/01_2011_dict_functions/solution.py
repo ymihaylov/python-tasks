04073956c9d957d61f8ebde6d5f4d338689d93ac
Yavor Mihaylov <yavor.st.m@gmail.com>
2018-04-11 00:27:21 +0300

    Update retturn

--- tasks/01_2011_dict_functions/solution.py
+++ tasks/01_2011_dict_functions/solution.py
@@ -1,15 +1,16 @@
 def make_multiset(l):
-    return(dict((i, l.count(i)) for i in l))
+    return dict((i, l.count(i)) for i in l)
 
 
 def ordered_dict(d):
-    return(sorted(d.items()))
+    return sorted(d.items())
 
 
 def reversed_dict(d):
-    return(dict(map(reversed, d.items())))
-    # return(dict((d[i], i) for i in d))
+    return dict((d[i], i) for i in d)
+    # return(dict(map(reversed, d.items())))
 
 
 def unique_objects(objects):
     return len(set(map(id, objects)))
+
