87d3a16fd67f566d50ba1ec5b6f88858feebd782
Yavor Mihaylov <yavor.st.m@gmail.com>
2018-10-03 23:21:32 +0300

    Update first notes of OOP

--- lectures_notes/4-OOP-1/4-2-notes.py
+++ lectures_notes/4-OOP-1/4-2-notes.py
@@ -13,13 +13,14 @@ type(42)  # Check the type of object
 
 
 class Vector:
-    # Constructor, doesnt return value (initializator)
+    # Constructor, doesn't return value (initializator)
     # First argument is always named self
-    # Atributes doesnt need declaration (Classes are open)
+    # Attributes doesn't need declaration (Classes are open)
     # But __init__ is not very first function that python invokes
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
 
 spam = Vector(1.0, 1.5)
 print(spam.x)
